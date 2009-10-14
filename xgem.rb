479adaa2b1ed5fa094eb8cfac9461c61544d3e0a
xianhua.zhou@gmail.com <zhouxh@zhouxh-laptop.(none)>
2009-10-14 23:15:53 +0800

    the first commit

---
+++ xgem.rb
@@ -0,0 +1,27 @@
+require 'rubygems'
+require 'rubygems/commands/uninstall_command.rb'
+
+gems = {}
+dep = Gem::Dependency.new '', Gem::Requirement.default
+Gem.source_index.search(dep).each do |index|
+	name = index.name
+	gems[name] = [] if gems[name].nil?
+	gems[name] << index.version.to_s
+end
+
+gems.each do |name, versions|
+	next if versions.size == 1
+	v0 = versions.sort!.pop
+	versions.each do |v|
+		begin
+			options = {:version => v}
+			Gem::Uninstaller.new(name, options).uninstall
+		rescue Exception => e
+			puts "can't remove \"#{name} v#{v}\": #{e}"
+		ensure
+			next
+		end
+	end
+end
+
+puts "Clean!"
